249f5d3a655f3c0c33f632d29f1cc9c4755622ad
BrettMcAdams96@gmail.com <brettmcadams96@gmail.com>
2020-08-24 13:19:51 -0400

    finished basic styling of the addTechitem component

--- src/components/addTechItem.js
+++ src/components/addTechItem.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import {
+    Container,
+    Row,
+    Col,
+    Button,
+    Alert,
+    Breadcrumb,
+    Navbar,
+    FormControl,
+    Nav,
+    NavDropdown,
+    Form,
+  } from "react-bootstrap";
+
 
 class AddTechItem extends Component {
-    state={
-        tech:{
-            dunno: 'dunno what structure you guys want'
+    state = {
+        tech: {
+            name: '',
+            descrip: '',
+            price: '',
         }
     }
-    onChange =(e) => {
+    onChange = (e) => {
         this.setState({
-            tech:{
+            tech: {
                 ...this.state.tech,
-                [e.target.name]:e.target.value
+                [e.target.name]: e.target.value
             }
         })
     }
-    
-    onSubmit= (e) => {
+
+    onSubmit = (e) => {
         e.preventDefault()
         // axiosCallgoesHere (post req)
         // set res to addTech(res..)
@@ -24,7 +41,31 @@ class AddTechItem extends Component {
     render() {
         return (
             <div>
-                
+                <Container>
+                    <Form>
+                        <Col>
+                            <Row>
+                                <Form.Group controlId="formName">
+                                    <Form.Label>Name</Form.Label>
+                                    <Form.Control type="text" placeholder="Your name goes here!" />
+                                </Form.Group>
+                            </Row>
+                            <Row>
+                                <Form.Group controlId="formPrice">
+                                    <Form.Label>Price</Form.Label>
+                                    <Form.Control type="text" placeholder="$0.00" />
+                                </Form.Group>
+                            </Row>
+                            <Row>
+                                <Form.Group controlId="formDescript">
+                                    <Form.Label>Item Description</Form.Label>
+                                    <Form.Control type="text" placeholder="Describe the item you are renting out" />
+                                </Form.Group>
+                            </Row>
+                        </Col>
+                        <Button variant="secondary" type="submit">Submit</Button>
+                    </Form>
+                </Container>
             </div>
         )
     }
